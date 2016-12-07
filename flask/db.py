fc32b850116094d51ced9c59ae372652ee3bb603
root <really0interesting@yahoo.co.jp>
2016-12-07 20:18:10 +0000

    edit

--- flask/db.py
+++ flask/db.py
@@ -5,12 +5,16 @@ import hashlib
 
 PATH = "db/app.db"
 
-def login(form):
-    connector = sqlite3.connect(PATH)
-    c  = connector.cursor()
+def hashing(form):
     form = dict(form)
     for key,value in form.items():
         form[key] = hashlib.sha256(value[0]).hexdigest()
+    return form
+
+def login(form):
+    connector = sqlite3.connect(PATH)
+    c  = connector.cursor()
+    form = hashing(form)
     sql = "select * from users where name = '%(username)s'"%form
     con = c.execute(sql)
     for row in con:
@@ -24,10 +28,15 @@ def login(form):
     return returner
 
 def create(form):
+    if login(form)[0]:
+        return False
+    if len(form["password"]) > 8:
+        return False
     con = sqlite3.connect(PATH)
     c = con.cursor()
-    sql = "insert into users(name,password) values(?,?)"
-    con = c.execute(sql,form)
+    form = hashing(form)
+    sql = "insert into users(name,password) values('%(username)s','%(password)s')"%form
+    con = c.execute(sql)
     con.commit()
     con.close()
 
